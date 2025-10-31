a119f8702021131b05c04afcddb3ba16595e34ce
ProSnigdho <hossain2snigdho@gmail.com>
2025-10-31 12:17:53 +0600

    Responsive

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -4,9 +4,9 @@ import { getFirestore, Firestore } from "firebase/firestore";
 import { getAnalytics, Analytics } from "firebase/analytics";
 import { getFunctions, Functions } from "firebase/functions";
 
-//env add
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
@@ -14,6 +14,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+console.log("FIREBASE API KEY:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 
 let app: FirebaseApp;
 let auth: Auth;
